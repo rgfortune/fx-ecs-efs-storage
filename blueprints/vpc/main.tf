def833ab85141c831547a320d2b411059c455591
Ricardo Fortune <ricardo.g.fortune@gmail.com>
2022-02-06 17:57:21 -0400

    Updating VPC module source

--- blueprints/vpc/main.tf
+++ blueprints/vpc/main.tf
@@ -5,11 +5,13 @@
 #
 # git::ssh://git@bitbucket_rgfortune/rgfortune/terraform-vpc_basic-module.git
 # git::ssh://git@bitbucket_rgfortune/rgfortune/terraform-vpc-module.git
+#
+# https://github.com/rgfortune/terraform-basic-vpc-module.git
 #------------------------------------------------
 
 
 module "vpc" {
-  source = "git::ssh://git@bitbucket_rgfortune/rgfortune/terraform-vpc_basic-module.git"
+  source = "https://github.com/rgfortune/terraform-basic-vpc-module.git"
 
   env      = var.env
   vpc_name = var.vpc_name
