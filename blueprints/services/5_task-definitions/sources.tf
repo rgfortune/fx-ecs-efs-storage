2f537312454f85d6dd8e704135e2f97f8e261879
Ricardo Fortune <ricardo.g.fortune@gmail.com>
2022-02-09 20:38:20 -0400

    VPC module state reference not needed for task definitions

--- blueprints/services/5_task-definitions/sources.tf
+++ blueprints/services/5_task-definitions/sources.tf
@@ -1,26 +1,4 @@
 #------------------------------------------------
-# VPC Module State Reference
-#------------------------------------------------
-
-#data "terraform_remote_state" "vpc" {
-#  backend = "s3"
-#
-#  # Paths are relative to the calling/root module.
-#  # {path.module} - making the path relative to the module where the expression exists.
-#
-#  config = {
-#    bucket = "ecs-fx-tfstate"
-#    key    = "ecs-fx-vpc.terraform.tfstate"
-#    region = "us-east-1"
-#  }
-#
-#  # https://www.terraform.io/docs/language/expressions/references.html#path-module
-#  # https://www.terraform.io/docs/language/functions/file.html
-#
-#}
-
-
-#------------------------------------------------
 # EFS State Reference
 #------------------------------------------------
 
